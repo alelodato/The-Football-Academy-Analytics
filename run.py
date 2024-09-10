477b1cd75f2afaa8a676c0544dafafa7fb001a3b
Alessio Lodato <alss.ldt@gmail.com>
2024-09-10 12:42:24 +0000

    add function to validate data insert by the user

--- run.py
+++ run.py
@@ -37,5 +37,21 @@ def get_weekly_revenue():
             break
         return weekly_data
     
-    
+    def validate_data(values):
+        """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 3 values.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 3:
+            raise ValueError(
+                f"Exactly 3 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+            print(f"Invalid data{e},please try again.\n")
+            return False
+
+    return True
     
